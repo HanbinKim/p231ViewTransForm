a7028c5109362c8a4d6caa2a4d99fa86f19904bd
HanbinKim <hottuc@naver.com>
2014-01-07 17:38:03 +0900

    p231ViewTransForm
    p231 뷰 트랜스폼 입니다..

--- p231ViewTransForm/ViewController.m
+++ p231ViewTransForm/ViewController.m
@@ -9,10 +9,72 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imgaeView;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)series:(id)sender
+{
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration: 0.2];
+	[UIView setAnimationDelegate:self];
+	[UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
+	self.imgaeView.frame = CGRectMake(0.0f, 0.0f, 160.0f, 200.0f);
+	[UIView commitAnimations];
+}
+
+-(void)animationDidStop:(NSString *)animationID finished:(NSNumber *)finished context:(void *)context
+{
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration: 0.2]; // 시간
+	self.imgaeView.frame = CGRectMake(100.0f, 0.0f, 160.0f, 200.0f);
+	[UIView commitAnimations];
+}
+
+- (IBAction)setOriginal:(id)sender
+{
+    [UIView beginAnimations:@"original" context:nil];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imgaeView.transform = CGAffineTransformIdentity;
+    
+    [UIView commitAnimations];
+}
+
+- (IBAction)combination:(id)sender
+{
+    CGAffineTransform t1 = CGAffineTransformMakeScale(1.2, 1.2);
+    CGAffineTransform t2 = CGAffineTransformMakeTranslation(30, 30);
+    CGAffineTransform comb = CGAffineTransformConcat(t1, t2);
+    CGAffineTransform t = CGAffineTransformConcat(self.imgaeView.transform, comb);
+    
+    [UIView beginAnimations:@"id_move" context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imgaeView.transform = t;
+    
+    [UIView commitAnimations];
+    /* 블록방식
+    [UIView animateWithDuration:0.7 animations:^{self.imgaeView.transform =t}];
+     */
+}
+
+- (IBAction)rotate:(id)sender
+{
+    [UIView beginAnimations:@"id_rotate" context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imgaeView.transform = CGAffineTransformRotate(self.imgaeView.transform, M_PI);
+    
+    [UIView commitAnimations];
+    
+    // 블록방식 [UIView animateWithDuration:0.7 animations:^{self.imgaeView.transform = CGAffineTransformRotate(self.imgaeView.transform, M_PI);}];
+}
+
 
 - (void)viewDidLoad
 {
